4421b7f56a84555487707e3d48086685c0e9ae23
Lιση <TigersSohn@live.de>
2020-03-04 20:44:04 +0100

    fix crash when deleting cars

--- client.lua
+++ client.lua
@@ -23,22 +23,23 @@ CreateThread(function()
 		end
 
         if currentCar > 0 then
-            local txdBack = GetTextureFromStreamedTxd("default", "back")
-            local txdBackTacho = GetTextureFromStreamedTxd("default", "back_tacho")
+		if DoesVehicleExist(currentCar) then
+		    local txdBack = GetTextureFromStreamedTxd("default", "back")
+		    local txdBackTacho = GetTextureFromStreamedTxd("default", "back_tacho")
 
-            local txdPin = GetTextureFromStreamedTxd("default", "pin")
-            currentSpeed = math.floor(GetCarSpeed(currentCar, _s) * 5 * 1.25)
-            currentRev = GetVehicleEngineRevs(currentCar)
-            if currentRev ~= false then currentRev = math.floor(((currentRev - deductRev) / 100000) * 1.4) else currentRev = 0 end
+		    local txdPin = GetTextureFromStreamedTxd("default", "pin")
+		    currentSpeed = math.floor(GetCarSpeed(currentCar, _s) * 5 * 1.25)
+		    currentRev = GetVehicleEngineRevs(currentCar)
+		    if currentRev ~= false then currentRev = math.floor(((currentRev - deductRev) / 100000) * 1.4) else currentRev = 0 end
 
-            DrawSprite(txdBack, 0.75, 0.87, 0.15, 0.15, 0, 255, 255, 255, 255)
-            DrawSprite(txdBackTacho, 0.90, 0.87, 0.15, 0.15, 0, 255, 255, 255, 255)
+		    DrawSprite(txdBack, 0.75, 0.87, 0.15, 0.15, 0, 255, 255, 255, 255)
+		    DrawSprite(txdBackTacho, 0.90, 0.87, 0.15, 0.15, 0, 255, 255, 255, 255)
 
-            DrawSprite(txdPin, 0.75, 0.87, 0.15, 0.15, ToFloat(currentSpeed), 255, 255, 255, 255)
-            DrawSprite(txdPin, 0.90, 0.87, 0.15, 0.15, ToFloat(currentRev), 255, 255, 255, 255)
+		    DrawSprite(txdPin, 0.75, 0.87, 0.15, 0.15, ToFloat(currentSpeed), 255, 255, 255, 255)
+		    DrawSprite(txdPin, 0.90, 0.87, 0.15, 0.15, ToFloat(currentRev), 255, 255, 255, 255)
 
-            currentGear = GetVehicleGear(currentCar)
-            if currentGear ~= false then currentGear = math.floor(GetVehicleGear(currentCar)) else currentGear = -1 end
-        end
+		    currentGear = GetVehicleGear(currentCar)
+		    if currentGear ~= false then currentGear = math.floor(GetVehicleGear(currentCar)) else currentGear = -1 
+		end
 	end
 end)
